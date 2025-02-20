dccc8804508fae154e02190cd2866043ea7d4abe
hornrei <hornrei0808@gmail.com>
2025-02-21 03:07:48 +0900

    logo設定

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('stock.index') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        <img src="{{ asset('image/logo.png') }}" alt="logo" class="h-8 w-auto">
                     </a>
                 </div>
 
